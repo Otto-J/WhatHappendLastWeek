64d6744e4693f9963e48c0dc9fb9258ff414faee
Otto <fa@ijust.cc>
2025-06-02 16:18:40 +0800

    fix: 添加前缀

--- scripts/downloadMp3sToLocal.ts
+++ scripts/downloadMp3sToLocal.ts
@@ -1,5 +1,5 @@
 import { execSync } from 'node:child_process'
-import { existsSync, mkdirSync } from 'node:fs'
+import { existsSync, mkdirSync, readdirSync, renameSync } from 'node:fs'
 import path from 'node:path'
 import debug from 'debug'
 import { fetchMp3 } from '../src/service/fetchMp3'
@@ -19,8 +19,15 @@ async function main() {
       log('media 目录不存在，创建', mediaDir)
       mkdirSync(mediaDir, { recursive: true })
     }
+
     // 将 media/week 目录下的所有文件移动到 temp-media 目录
     execSync(`mv ${mediaDir}/* ${TEMP_MEDIA_DIR}`)
+    // 循环 media/week 所有文件，文件开头添加 week_ 前缀
+    const files = readdirSync(TEMP_MEDIA_DIR)
+    for (const file of files) {
+      // renameSync(path.join(mediaDir, file), path.join(TEMP_MEDIA_DIR, `week_${file}`))
+      renameSync(path.join(TEMP_MEDIA_DIR, file), path.join(TEMP_MEDIA_DIR, `${week}_${file}`))
+    }
   }
   else {
     console.log('下载失败', res.msg)
